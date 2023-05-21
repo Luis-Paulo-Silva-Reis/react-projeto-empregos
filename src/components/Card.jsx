effa2fcb3f33dc4965ae056d4d49f671f0b42cc9
Luis Paulo Silva Reis <48827159+Luis-Paulo-Silva-Reis@users.noreply.github.com>
2023-05-21 15:45:08 -0300

    css update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import "../styles/Card.css";
 
 const Card = ({ item, index }) => (
   <div className="card" key={index}>
     <h3>{item.titulo}</h3>
-    <p>{item.descricao}</p>
+    <p>Descricao: {item.descricao}</p>
+    <div className="company-info">
+      <p className="location">
+        <strong>Localização:</strong> {item.localizacao}
+      </p>
+      <p>
+        <strong>Empresa:</strong> {item.empresa}
+      </p>
+      <p>
+        <strong>Tipo de Emprego:</strong> {item.tipo_emprego}
+      </p>
+      <p className="salary">
+        <strong>Salário:</strong> {item.salario}
+      </p>
+    </div>
+    <p className="additional-requirements">
+      <strong>Requisitos Adicionais:</strong> {item.requisitos_adicionais}
+    </p>
+    <p className="experience">
+      <strong>Experiência Mínima:</strong> {item.experiencia_minima}
+    </p>
+    <p className="date">
+      <strong>Data de Publicação:</strong> {item.data_publicacao}
+    </p>
+    <p className="date">
+      <strong>Data de Expiração:</strong> {item.data_expiracao}
+    </p>
+    <p className="application-link">
+      <strong>Link de Aplicação:</strong>{" "}
+      <a href={item.link_aplicacao}>{item.link_aplicacao}</a>
+    </p>
+    <p className="status">
+      <strong>Status:</strong> {item.status}
+    </p>
   </div>
 );
 
--- src/components/JobPosting.jsx
+++ src/components/JobPosting.jsx
@@ -2,13 +2,22 @@ import React, { useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import "../styles/JobPosting.css";
-import JobCard from "./JobCard";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const JobPosting = () => {
   const [titulo, setTitulo] = useState("");
   const [descricao, setDescricao] = useState("");
+  const [empresa, setEmpresa] = useState("");
+  const [localizacao, setLocalizacao] = useState("");
+  const [tipoEmprego, setTipoEmprego] = useState("");
+  const [salario, setSalario] = useState("");
+  const [requisitosAdicionais, setRequisitosAdicionais] = useState("");
+  const [experienciaMinima, setExperienciaMinima] = useState("");
+  const [dataPublicacao, setDataPublicacao] = useState("");
+  const [dataExpiracao, setDataExpiracao] = useState("");
+  const [linkAplicacao, setLinkAplicacao] = useState("");
+  const [status, setStatus] = useState("");
   const [vagas, setVagas] = useState([]);
 
   const handleSubmit = async (e) => {
@@ -18,7 +27,20 @@ const JobPosting = () => {
       const token = Cookies.get("Authorization"); // Obtenha o token dos cookies ou localStorage
       const response = await axios.post(
         "https://talentsync.click:8080/jobsposting",
-        { titulo, descricao },
+        {
+          titulo,
+          descricao,
+          empresa,
+          localizacao,
+          tipoEmprego,
+          salario,
+          requisitosAdicionais,
+          experienciaMinima,
+          dataPublicacao,
+          dataExpiracao,
+          linkAplicacao,
+          status,
+        },
         {
           headers: {
             Authorization: `${token}`,
@@ -27,10 +49,20 @@ const JobPosting = () => {
       );
 
       // Adicione a nova vaga à lista de vagas
-      setVagas([...vagas, { titulo, descricao }]);
+      setVagas([...vagas, { titulo, descricao, empresa, localizacao, tipoEmprego, salario, requisitosAdicionais, experienciaMinima, dataPublicacao, dataExpiracao, linkAplicacao, status }]);
 
       setTitulo("");
       setDescricao("");
+      setEmpresa("");
+      setLocalizacao("");
+      setTipoEmprego("");
+      setSalario("");
+      setRequisitosAdicionais("");
+      setExperienciaMinima("");
+      setDataPublicacao("");
+      setDataExpiracao("");
+      setLinkAplicacao("");
+      setStatus("");
 
       alert("Vaga inserida com sucesso!");
     } catch (error) {
@@ -61,6 +93,105 @@ const JobPosting = () => {
               onChange={(e) => setDescricao(e.target.value)}
               required
             ></textarea>
+          </div>
+          <div>
+            <label htmlFor="empresa">Empresa:</label>
+            <input
+              type="text"
+              id="empresa"
+              value={empresa}
+              onChange={(e) => setEmpresa(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="localizacao">Localização:</label>
+            <input
+              type="text"
+              id="localizacao"
+              value={localizacao}
+              onChange={(e) => setLocalizacao(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="tipoEmprego">Tipo de Emprego:</label>
+            <input
+              type="text"
+              id="tipoEmprego"
+              value={tipoEmprego}
+              onChange={(e) => setTipoEmprego(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="salario">Salário:</label>
+            <input
+              type="text"
+              id="salario"
+              value={salario}
+              onChange={(e) => setSalario(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="requisitosAdicionais">Requisitos Adicionais:</label>
+            <textarea
+              id="requisitosAdicionais"
+              value={requisitosAdicionais}
+              onChange={(e) => setRequisitosAdicionais(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="experienciaMinima">Experiência Mínima:</label>
+            <input
+              type="number"
+              id="experienciaMinima"
+              value={experienciaMinima}
+              onChange={(e) => setExperienciaMinima(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="dataPublicacao">Data de Publicação:</label>
+            <input
+              type="date"
+              id="dataPublicacao"
+              value={dataPublicacao}
+              onChange={(e) => setDataPublicacao(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="dataExpiracao">Data de Expiração:</label>
+            <input
+              type="date"
+              id="dataExpiracao"
+              value={dataExpiracao}
+              onChange={(e) => setDataExpiracao(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="linkAplicacao">Link de Aplicação:</label>
+            <input
+              type="text"
+              id="linkAplicacao"
+              value={linkAplicacao}
+              onChange={(e) => setLinkAplicacao(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="status">Status:</label>
+            <input
+              type="text"
+              id="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              required
+            />
           </div>
           <button type="submit">Enviar</button>
         </form>
